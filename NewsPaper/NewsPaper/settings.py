686bb2aa3f05d03ebc050362deae12314728af7e
Kostya <mltosk777@yandex.ru>
2025-10-27 12:57:36 +0300

    add Redis

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -137,4 +137,13 @@ CRONJOBS = [
     ('0 9 * * 1', 'news.cron.weekly_newsletter'),  # каждый понедельник в 09:00
 ]
 
+# Настройки Celery
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Moscow'
+
+
 
